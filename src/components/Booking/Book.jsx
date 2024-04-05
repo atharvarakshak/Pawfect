2b5db645c439bac1bd63d32f4462075dd149f4ba
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-04-06 00:45:05 +0530

    update home

---
+++ src/components/Booking/Book.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Book = () => {
+  return (
+    <div>
+        <div className="">
+           <p className="">Book an appointment</p>
+           <input type="full Name" className="" />
+           <input type="full Name" className="" />
+           <input type="full Name" className="" />
+        </div>
+    </div>
+  )
+}
+
+export default Book
