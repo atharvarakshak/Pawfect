342a2567c5e85ec3ef9511093644bf5802887017
atharvarakshak <atharvarakshak23@gmail.com>
2024-04-06 01:02:48 +0530

    updates

---
+++ src/components/Service/Services.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import i1 from "../../assets/i1.svg"
+import i2 from "../../assets/i2.svg"
+import i3 from "../../assets/i3.svg"
+import i4 from "../../assets/i4.svg"
+import i5 from "../../assets/i5.svg"
+import i6 from "../../assets/i6.svg"
+function Services() {
+  return (
+    <div className=' flex md:flex-col border-2 border-gray-300 mx-auto rounded-lg shadow-lg'>
+      {/* Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti quisquam tempora alias possimus beatae voluptatem, veritatis assumenda exercitationem et! Adipisci cum dicta voluptatum? Quidem esse facere dolores, quibusdam ut reprehenderit fugiat architecto doloremque nostrum eum recusandae fugit perspiciatis? Reprehenderit quae voluptates exercitationem nisi fugiat iste deserunt at dolorem obcaecati sapiente quod mollitia excepturi, repudiandae veniam, et odio culpa, eligendi nemo assumenda. Iusto perferendis doloribus eum praesentium asperiores corporis voluptatum totam quod! Fugit rerum maiores dolorum, necessitatibus voluptatem, amet ducimus atque nihil perferendis earum culpa. Commodi, blanditiis esse illo eveniet voluptate sit nulla culpa earum inventore alias iusto nesciunt mollitia minima? */}
+    <div className='flex flex-col md:flex-row justify-evenly'>
+        <img className='h-50 w-50' src={i1} alt="" />
+        <img className='h-50 w-50' src={i2} alt="" />
+        <img className='h-50 w-50' src={i3} alt="" />
+    </div>
+    <div className='flex flex-col md:flex-row justify-evenly'>
+        <img className='h-50 w-50' src={i4} alt="" />
+        <img className='h-50 w-50' src={i5} alt="" />
+        <img className='h-50 w-50' src={i6} alt="" />
+    </div>
+</div>
+
+  )
+}
+
+export default Services
