5852580b7cbfae5ab7eabf593c8883c591279627
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-04-06 09:55:28 +0530

    -

--- src/components/Booking/Book.jsx
+++ src/components/Booking/Book.jsx
@@ -3,17 +3,21 @@ import React from 'react'
 const Book = () => {
   return (
     <div>
-        <div className="bg-orange-300 flex flex-col gap-9 lg:w-[578px] lg:h-[544px] m-auto mt-8 rounded-2xl p-10">
+        <div className="bg-orange-300 flex flex-col gap-9 lg:w-[578px] lg:h-[544px] m-auto rounded-2xl p-10 mt-32">
            <p className="text-center font-bold text-3xl ">Book an appointment !</p>
            <input type="full Name" className=" p-2 rounded-lg" placeholder='Full Name'/>
            <input type="full Name" className="p-2 rounded-lg" placeholder='Address'/>
            <input type="full Name" className="p-2 rounded-lg" placeholder='Phone No.'/>
            <input type="full Name" className="p-2 rounded-lg" placeholder='selected service'/>
 
-           <div className="">
+           <div className="text-xl flex mx-5 justify-between">
             <p className="">Estimated Cost:</p>
-            <p className=""></p>
+            <p className="">Rs:</p>
            </div>
+
+           <a href="" className="">
+            
+           </a>
         </div>
     </div>
   )
